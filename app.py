38483463417d788c49e1b90143b6ec87a25fcea5
Sherry Yang <sherryyang@Sherrys-MacBook-Pro.local>
2020-05-10 14:20:28 +0800

    added basic templates

---
+++ app.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 14:57:17 2020
+
+@author: etill
+"""
+
+#import statements
+from flask import Flask, render_template
+
+#Flask app variable
+app = Flask(__name__)
+
+#static route
+@app.route("/")
+def test():
+    return render_template("index.html")
+
+@app.route("/1006")
+def test123():
+    return render_template("1006.html")
+
+@app.route("/columbia")
+def columbia():
+    return "Columbia!!"
+
+#start the server
+if __name__ == "__main__":
+    app.run()
